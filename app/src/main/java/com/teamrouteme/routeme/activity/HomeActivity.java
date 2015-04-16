47af56777ebbe05d0b627c2cecd894a8f5cbae31
ginofarisano <gino.farisano@gmail.com>
2015-04-16 17:06:34 +0200

    Basic interface of the project

---
+++ app/src/main/java/com/teamrouteme/routeme/activity/HomeActivity.java
@@ -0,0 +1,207 @@
+package com.teamrouteme.routeme.activity;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.Button;
+
+import com.teamrouteme.routeme.fragment.NavigationDrawerFragment;
+import com.teamrouteme.routeme.R;
+
+
+public class HomeActivity extends ActionBarActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_home);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.cercaItinerario);
+                break;
+            case 2:
+                mTitle = getString(R.string.nomeCognome);
+                break;
+            case 3:
+                mTitle = getString(R.string.creaItinerario);
+                break;
+            case 4:
+                mTitle = getString(R.string.mieiItinerari);
+                break;
+            case 5:
+                mTitle = getString(R.string.itinerariAcquistati);
+                break;
+            case 6:
+                mTitle = getString(R.string.iMieiDesideri);
+                break;
+            case 7:
+                mTitle = getString(R.string.logOut);
+                break;
+
+
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.home, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         *
+         * la soluzione qui proposta è provvisoria
+         * va creato una classe per ogni fragment
+         *
+         * @param inflater
+         * @param container
+         * @param savedInstanceState
+         * @return
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            Bundle args = getArguments();
+
+            //sezione scelta
+            int number=args.getInt(ARG_SECTION_NUMBER);
+
+            View rootView=null;
+
+            switch (number) {
+                case 1:
+                    rootView = inflater.inflate(R.layout.fragment_search, container, false);
+                    break;
+                case 2:
+                    rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+                    break;
+                case 3:
+                    //crea itinerario
+                    rootView = inflater.inflate(R.layout.fragment_crea_tinerario, container, false);
+                break;
+                case 4:
+                    rootView = inflater.inflate(R.layout.fragment_itinerari_caricati, container, false);
+                break;
+                case 5:
+                    rootView = inflater.inflate(R.layout.fragment_itinerari_scaricati, container, false);
+                break;
+                case 6:
+                    rootView = inflater.inflate(R.layout.fragment_desideri, container, false);
+                break;
+                case 7:
+                    rootView = inflater.inflate(R.layout.fragment_login, container, false);
+                    break;
+        }
+
+            return rootView;
+
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((HomeActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+}

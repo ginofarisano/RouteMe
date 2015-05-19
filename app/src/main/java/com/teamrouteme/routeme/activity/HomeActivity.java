73f707947cc10ffbe89bc4d9c4c4bd2fab25f64a
nicolopergola <nico.pergola90@gmail.com>
2015-05-19 16:04:49 +0200

    aggiunta icona e navigation drawer penso definitiva

--- app/src/main/java/com/teamrouteme/routeme/activity/HomeActivity.java
+++ app/src/main/java/com/teamrouteme/routeme/activity/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.parse.ParseUser;
@@ -34,6 +35,10 @@ public class HomeActivity extends MaterialNavigationDrawer {
     //La borra
     @Override
     public void init(Bundle bundle) {
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
         currentUser = ParseUser.getCurrentUser();
 
